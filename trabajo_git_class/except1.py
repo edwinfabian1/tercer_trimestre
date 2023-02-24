31172f934e93b5cec235e58215cfbc00455a7ad6
edwinfabian1 <edwinfvillamil@gmail.com>
2023-02-24 17:40:58 -0500

    excepciones

---
+++ trabajo_git_class/except1.py
@@ -0,0 +1,28 @@
+#def divisores(num):
+#     for i in range(num):
+#         if num%i==0:
+#             print(i,' es divisor')
+
+# try:
+#     divisores(19)
+# except:
+
+def divisores(num):
+
+    if type(num)!=int:                    #el if nos dice si el operador no es igual imprima 
+        print('Solo trabaja con numeros')   
+    else:                                
+        try:
+            for i in range(1,num):           
+                if num%i==0:
+                    print(i,' es divisor')
+        except ZeroDivisionError:
+            print('Indeterminacion')
+        except TypeError:
+            print('No ingreso un numero')
+        except:
+            print('Error no determinado')
+
+divisores([])
+print('El programa continua desde aqui')
+
---
+++ trabajo_git_class/exceptA.py
@@ -0,0 +1,7 @@
+try:
+    #print(1/1))
+    raise SyntaxError
+except SyntaxError:
+    print('Cierra el parentesis') 
+
+#El interprete de python permite definir el error 
